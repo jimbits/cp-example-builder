d508f60c4ebb12716a47b900b040838d4f68eb01
jimbits <jimbits@jimhortons.com>
2024-01-14 12:17:52 -0700

    update assets

--- js/index.js
+++ js/index.js
@@ -1,18 +1,1 @@
-import hljs from "highlight.js/lib/core";
-import javascript from "highlight.js/lib/languages/javascript";
-import { clipboard } from "./utils/clipboard";
-import "highlight.js/styles/atom-one-dark.css";
-
-hljs.registerLanguage("javascript", javascript);
-
-document.addEventListener("DOMContentLoaded", onPageLoaded);
-
-function onPageLoaded(e) {
-	document.querySelectorAll("pre").forEach((el) => {
-		hljs.highlightElement(el);
-	});
-}
-
-const temp = await clipboard();
-console.log(temp);
-/*  */
+console.log("javascript file attached to web docuement");
